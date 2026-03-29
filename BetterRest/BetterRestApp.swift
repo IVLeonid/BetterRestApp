1dafb10a4f5d45fb2677ff6e62f183121a2137a1
Lenya <leonidivanov826@gmail.com>
2026-03-29 23:07:54 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Леонід Іванов on 29.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
